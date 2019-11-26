56236e5e8a700957274b2f2bd5e173f5628d7d38
Hector Castillo-Martinez <hcastillomartinez@cs.unm.edu>
2019-11-25 22:00:39 -0700

    Update main.c

--- main.c
+++ main.c
@@ -6,10 +6,10 @@
 #include <sys/types.h>
 
 #define START 32400
+// debugging end (12 minutes)
 #define END 33120
 // #define END 68400
 #define MAX_QUEUE 800
-static int done = 0;
 static int current_time = START;
 static int incoming_user = 0;
 static int total_incoming = 0;
@@ -18,8 +18,11 @@ static int time_step = 0;
 static int queue = 0;
 static int maximum_queue = 0;
 static int max_q_time = 0;
+static int max_passengers = 7;
+static int total_rode = 0;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t car_cond = PTHREAD_COND_INITIALIZER;
 
 /*
 Used to generate the # of incoming people based
@@ -45,15 +48,21 @@ Just increments time as fast as it can at the moment
 does not really map virtual minute to real second.
 */
 void *time_passage(){
+  sleep(1);
   while(current_time < END){
+
     pthread_mutex_lock(&lock);
+    printf("time enter\n");
     int hours = current_time/3600;
     int min = (current_time%3600)/60;
     int secs = (current_time%3600)%60;
     // printf("%d:%d:%d\n",hours,min,secs);
     current_time+=60;
     time_step++;
+    pthread_cond_signal(&car_cond);
     pthread_cond_signal(&cond);
+    // pthread_cond_signal(&car_cond);
+    printf("p exit\n");
     pthread_mutex_unlock(&lock);
     sleep(1);
   }
@@ -67,6 +76,7 @@ void *incomingP_handler(){
   while(current_time < END){
     pthread_mutex_lock(&lock);
     pthread_cond_wait(&cond,&lock);
+    printf("p enter\n");
     int hours = current_time/3600;
     int min = (current_time%3600)/60;
     int secs = (current_time%3600)%60;
@@ -92,6 +102,8 @@ void *incomingP_handler(){
         maximum_queue = queue;
       }
     }
+    printf("p exit\n");
+    pthread_cond_signal(&car_cond);
     pthread_mutex_unlock(&lock);
     // sleep(1);
   }
@@ -103,23 +115,50 @@ void *incomingP_handler(){
   Handles the things that cars needs to do.
 */
 void *carHandler(){
+
+  while(current_time < END){
+    pthread_mutex_lock(&lock);
+    pthread_cond_wait(&car_cond, &lock);
+    printf("enter\n");
+    int diff = queue - max_passengers;
+    if(diff >= 0){
+      queue -= max_passengers;
+      total_rode += max_passengers;
+    }
+    else{
+      queue -= queue;
+      total_rode += max_passengers;
+    }
+    printf("exit\n");
+    pthread_mutex_unlock(&lock);
+  }
+
   return NULL;
 }
 
 
 int main(void) {
+  int n = 3;
   pthread_t tid1;
   pthread_t tid2;
-  pthread_mutex_init(&lock, NULL);
+  pthread_t cars[n];
+  // pthread_mutex_init(&lock, NULL);
   pthread_create(&tid1,NULL,time_passage,(void *)NULL);
   pthread_create(&tid2,NULL,incomingP_handler,(void *)NULL);
-  pthread_join(tid2, NULL);
+  for(int i = 0; i < n; i ++){
+    pthread_create(&cars[i], NULL, carHandler, (void *) carHandler);
+  }
   pthread_join(tid1, NULL);
+  pthread_join(tid2, NULL);
+  for(int i = 0 ;i < n;i++){
+    pthread_join(cars[i], NULL);
+  }
+  // pthread_join(tid1, NULL);
   pthread_mutex_destroy(&lock);
   pthread_cond_destroy(&cond);
   int hours = max_q_time/3600;
   int min = (max_q_time%3600)/60;
   int secs = (max_q_time%3600)%60;
-  printf("Total Number of Guests: %d, Total Number of People on Ride: %d, Total Number of People Rejected: %d, Average Waiting Time: %d, Maximum Length of Line for Day and Time of Occurence: %d at %d:%d:%d\n", total_incoming, 0, total_rejected, time_step, maximum_queue, hours, min, secs);
+  printf("\nTotal Number of Guests: %d, Total Number of People on Ride: %d, Total Number of People Rejected: %d, Average Waiting Time: %d, Maximum Length of Line for Day and Time of Occurence: %d at %d:%d:%d\n", total_incoming, total_rode, total_rejected, time_step, maximum_queue, hours, min, secs);
   return 0;
 }
